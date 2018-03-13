0d85e34ee94582747f556f312b2483c90971e85c
Aparna Jha <aparnajha482@gmail.com>
2018-03-13 23:00:27 +0530

    a subclass(Media)  created.

--- movie.py
+++ movie.py
@@ -2,18 +2,18 @@ import webbrowser
 
 
 class Movie:
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
 
+class Media(Movie):
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
 toy_story =Movie("Toy story","story of a boy and his toys that comes to life",
                                     "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                                     "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
 
 
 
